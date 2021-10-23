d2f407d94d213704f526ede044fd57a401473586
Oscar00001 <oscardias0001@gmail.com>
2021-10-23 16:05:16 -0400

    Initial commit

---
+++ src/test/java/com/oscar/ovewatchdashboard/OvewatchDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscar.ovewatchdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvewatchDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
